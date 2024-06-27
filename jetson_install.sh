a5a8cb1a76674e68ec2785c5ebf17a9fe7847056
rhysdg <rhysdgwilliams@gmail.com>
2024-06-27 08:23:19 -0400

    adding jetson installation scripts

---
+++ jetson_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install -y g++-11
+
+sudo apt-get install python3-pip
+sudo apt-get install libopenblas-base libopenmpi-dev libomp-dev portaudio19-dev espeak ffmpeg libespeak1 python3-pyaudio
+
+###grabbing all relevant wheels
+wget https://nvidia.box.com/shared/static/g74cjqh8fcyd9faobm7yeif8mmxdvc0g.whl -O onnxruntime_gpu-1.16.0-cp311-cp311-linux_aarch64.whl
+pip install onnxruntime_gpu-1.16.0-cp311-cp311-linux_aarch64.whl
+
+echo "done!"
